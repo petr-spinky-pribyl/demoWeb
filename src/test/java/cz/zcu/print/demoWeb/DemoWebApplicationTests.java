383c8de51f9a7e36e2f94331fef1f535846c87b9
SPI <petr.pribyl@cca.cz>
2021-09-21 22:14:23 +0200

    Initial commit

---
+++ src/test/java/cz/zcu/print/demoWeb/DemoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.zcu.print.demoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
